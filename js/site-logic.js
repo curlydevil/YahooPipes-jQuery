9036158c7c6c5e928bde6657f81b1656f11ca53f
d.berzoy <d.berzoy@gmail.com>
2015-06-26 14:18:31 +0300

    naming issue resolved

--- js/site-logic.js
+++ js/site-logic.js
@@ -1,5 +1,5 @@
 var pipesUrl = 'http://pipes.yahoo.com/pipes/pipe.run?_id=e9a2e77dffb3205d035c4e311d77bbe6&_render=json';
-var templateUrl = 'Templates/nnewsPostTemplate.html';
+var templateUrl = 'Templates/newsPostTemplate.html';
 var pageMotto = $('#page-motto');
 var newsContainer = $('#data-host');
 
