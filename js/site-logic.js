0d1764e9886787eadfd855e77ccf9a60f4f67ccf
d.berzoy <d.berzoy@gmail.com>
2015-06-26 14:17:52 +0300

    last page casing issue

--- js/site-logic.js
+++ js/site-logic.js
@@ -1,5 +1,5 @@
 var pipesUrl = 'http://pipes.yahoo.com/pipes/pipe.run?_id=e9a2e77dffb3205d035c4e311d77bbe6&_render=json';
-var templateUrl = 'Templates/NewsPostTemplate.html';
+var templateUrl = 'Templates/nnewsPostTemplate.html';
 var pageMotto = $('#page-motto');
 var newsContainer = $('#data-host');
 
